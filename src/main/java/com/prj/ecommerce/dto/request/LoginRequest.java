d364ccd14f571733c507fdaa6b86b4fa447a500e
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2025-11-28 23:04:08 +0700

    add validation

--- src/main/java/com/prj/ecommerce/dto/request/LoginRequest.java
+++ src/main/java/com/prj/ecommerce/dto/request/LoginRequest.java
@@ -1,10 +1,14 @@
 package com.prj.ecommerce.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class LoginRequest {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
--- src/main/java/com/prj/ecommerce/dto/request/RegisterRequest.java
+++ src/main/java/com/prj/ecommerce/dto/request/RegisterRequest.java
@@ -1,13 +1,24 @@
 package com.prj.ecommerce.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class RegisterRequest {
+    @NotBlank
     private String fullName;
+    @NotBlank
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 20)
     private String password;
+    @NotBlank
+    @Pattern(regexp = "^(0[0-9]{9})$")
     private String phoneNumber;
+    @Email
     private String email;
 }
