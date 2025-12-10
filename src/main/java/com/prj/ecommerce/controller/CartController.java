a07edf43979132763d7ccbecb887859333bae3cd
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2025-12-10 10:52:08 +0700

    crud cart item

--- src/main/java/com/prj/ecommerce/controller/CartController.java
+++ src/main/java/com/prj/ecommerce/controller/CartController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/cart")
@@ -17,6 +19,9 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping
+    public List<AddCartItemResponse> findAll() {
+        return cartService.getCartItems();
+    }
 
 
     @PostMapping
