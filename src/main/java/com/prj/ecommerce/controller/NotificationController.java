7d02922a5f25e8389d5a750c058b090564cb39b3
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2026-01-14 20:13:37 +0700

    notification page

---
+++ src/main/java/com/prj/ecommerce/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package com.prj.ecommerce.controller;
+
+import com.prj.ecommerce.common.ReferenceType;
+import com.prj.ecommerce.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping("/user/notifications")
+    public String notifications(Model model,
+                                @RequestParam(required = false) ReferenceType referenceType) {
+        model.addAttribute("notifications", notificationService.getAllNotifications(referenceType));
+        return "notifications";
+    }
+}
